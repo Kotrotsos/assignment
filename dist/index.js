f687a3f07ed0dd7e4e312c8c45ffdcafc8775b30
Marco Kotrotsos <kotrotsos@gmail.com>
2018-08-27 11:23:17 +0200

    little more wrapup

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// This should load the jQuery plugin
 $(function () {
 
   /* Options:
@@ -8,6 +7,7 @@ $(function () {
      datasource: the endpoint where the data should come from.
      When datasource is not given here. It's expected on the HTML element as an attribute data-datasource
      This way you can have multiple carousels using different datasources. */
+
   var options = {
     template: "carousel-template",
     datasource: "https://wt-83e58c9f13b3df3a68adfe5263e85725-0.sandbox.auth0-extend.com/TNT-Testimonials"
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
+"use strict";
 
-
-// This should load the jQuery plugin
 $( () => {
 
   /* Options:
@@ -8,6 +7,7 @@ $( () => {
      datasource: the endpoint where the data should come from.
      When datasource is not given here. It's expected on the HTML element as an attribute data-datasource
      This way you can have multiple carousels using different datasources. */
+
   let options = {
     template: "carousel-template",
     datasource: "https://wt-83e58c9f13b3df3a68adfe5263e85725-0.sandbox.auth0-extend.com/TNT-Testimonials"
